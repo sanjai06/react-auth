0650cb7b062970304a6b5a410017eb89c355b9b2
sanjaidebuger <nsksanjai@gmail.com>
2022-08-30 14:03:53 +0530

    first 1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import './App.css';
+
+
+import Login from './components/login/Login.jsx';
+import Dashboard from './components/Dashboard';
+import Maincontent from './components/Maincontent';
+
+ 
+function App() {
+
+const [token, setToken] = useState();
+ 
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div>
+      <h1 className='tittle'>Welcome</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={ <Dashboard /> } />
+          <Route path='/maincontent' element={ <Maincontent /> } />
+        </Routes>
+      </BrowserRouter>
+
+
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Maincontent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Maincontent() {
+  return (
+    <div><h1>Maincontent</h1></div>
+  )
+}
+
+export default Maincontent
